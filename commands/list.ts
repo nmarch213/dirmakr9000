5c57a8964489cedebca68b28c5269d6aa1018621
Nicholas March <rival@passion.lan>
2024-07-20 20:17:40 -0700

    remove todo

--- commands/list.ts
+++ commands/list.ts
@@ -6,7 +6,6 @@ export const listDirs = (state: Dirs): void => {
   console.log("------------------------------");
 };
 
-// TODO: for each nested dir, print the dir name with one tab space more than the parent dir
 const listNestedDirs = (state: Dirs, buffer: string): void => {
   buffer = buffer || "";
   Object.keys(state).forEach((key) => {
