40272b2f71df18a040f022128d959b62b08ffb52
Nicholas March <rival@passion.lan>
2024-07-20 18:44:01 -0700

    remove log

--- create.ts
+++ create.ts
@@ -2,7 +2,6 @@ import { Dirs } from "./models/dirs.model";
 
 export const createDir = (dir: string, state: Dirs): any => {
   if (state[dir]) {
-    console.log(`Directory ${dir} already exists`);
     return state;
   } else {
     return {
