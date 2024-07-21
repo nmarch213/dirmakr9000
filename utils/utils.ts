ffe7a5e382b9ac1548fe56e3612805bae2f53e65
Nicholas March <rival@passion.lan>
2024-07-20 20:46:30 -0700

    check things

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,8 +1,18 @@
 import { Dirs } from "../models/dirs.model";
 
 export const findDir = (dir: string[], state: Dirs): Dirs => {
+  if (!state) {
+    return state;
+  }
+  if (dir.length === 0) {
+    return state;
+  }
   if (dir.length === 1) {
     return state[dir[0]];
   }
   return findDir(dir.slice(1), state[dir[0]]);
+};
+
+export const deepCopy = (value: any): any => {
+  return JSON.parse(JSON.stringify(value));
 };
